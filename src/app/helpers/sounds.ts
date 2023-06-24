12c25911f3ba06e2f31bf9a6fb1caf8ce44dc2b6
Troy <viperaus@gmail.com>
2023-06-24 16:29:52 +1000

    Break out sounds for easier updating

---
+++ src/app/helpers/sounds.ts
@@ -0,0 +1,6 @@
+export const sounds = [
+  { id: 1, file: "/audio/blurrrurp.mp3", name: "Blurp", length: 850 },
+  { id: 2, file: "/audio/dududah.mp3", name: "DuDah", length: 2000 },
+  { id: 3, file: "/audio/correct.mp3", name: "Correct", length: 500 },
+  { id: 4, file: "/audio/wrong.mp3", name: "Wrong", length: 350 },
+];
